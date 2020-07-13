68128761c81beb898aaf17a7f2d41b9993c0680b
elenapanasyuk <elena.panasyuk@gmail.com>
2020-07-13 22:25:44 +0300

    Updates

--- src.bc1e03b8.js
+++ src.bc1e03b8.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/menu.js");
 },{"./sass/main.scss":"clu1","./js/menu.js":"i0CD"}]},{},["Focm"], null)
-//# sourceMappingURL=/group-10-barbershop/src.f957b951.js.map
+//# sourceMappingURL=/group-10-barbershop/src.bc1e03b8.js.map
