7b4bbecf7029af76445bd1e86469d7216dd2a781
Kateryna Mysechko <katerynamysechko@gmail.com>
2020-07-18 11:23:53 +0300

    Header: додає JS  до моб меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,19 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  menuBtnRef.addEventListener('click', () => {
+  const body = document.querySelector('body');
+  const toggleMenu = () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     menuBtnRef.classList.toggle('is-open');
+    body.classList.toggle('body-hidden');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
-    document.body.classList.toggle('menu-open');
+  };
+  menuBtnRef.addEventListener('click', () => {
+    toggleMenu();
+  });
+  mobileMenuRef.addEventListener('click', ({ target }) => {
+    target.getAttribute('data-menu-link') === '' && toggleMenu();
   });
 })();
